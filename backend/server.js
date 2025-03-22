9f2e071dfded31c1cbf95238c34b3b2c17b5c6fe
AnshKumar6400 <anshkumar6400@gmail.com>
2025-03-22 21:14:38 +0530

    Fixed backend issues

--- backend/server.js
+++ backend/server.js
@@ -129,4 +129,4 @@ app.get('/api/user-profile', authenticate, (req, res) => {
 
 // Start the Server
 const PORT =process.env.PORT|| 5005;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
